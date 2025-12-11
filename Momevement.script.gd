24e679904a7dbb00b54a017a6f38848cbfc29e85
R-programmeu <raphaelbourgault9@gmail.com>
2025-12-10 22:13:29 -0300

    Raphaël 2(10 decembre 2025)

--- Momevement.script.gd
+++ Momevement.script.gd
@@ -6,7 +6,10 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var neck := $neck
+@onready var camera := $Neck/Camera3D
 
+func _unhandled_input(envent):
 
 func _physics_process(delta):
 	# Add the gravity.
