dbd21efdc09ca5f87a9a879851cf42b84051c5a4
olivierlaflamme18-dot <Olivierlaflamme18@gmail.com>
2026-05-27 20:06:07 -0400

    git peut avoir infini fichier,non?

---
+++ PLAY NORMAL MODE.gd
@@ -0,0 +1,12 @@
+extends Button
+
+func _pressed():
+	get_tree().change_scene_to_file("res://devinette.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
