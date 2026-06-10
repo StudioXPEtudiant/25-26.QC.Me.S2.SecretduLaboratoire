c220b659c61c7e8a0eb94d49e6a2ee845cbf7ea1
olivierlaflamme18-dot <Olivierlaflamme18@gmail.com>
2026-06-10 19:19:51 -0400

    test

--- Boulet de cannon.gd
+++ Boulet de cannon.gd
@@ -1,28 +1,36 @@
 extends MeshInstance2D
 
-var cible:Vector2
+var cible :Vector2 = Vector2(0, 0)
+var est_un_clone : bool = false
+var peut_tirer:bool = true
 
 func _pressed():
 	var zone = get_tree().get_first_node_in_group("zone_canon")
-	cible = Vector2(get_local_mouse_position())
 	if zone != null:
 		var clone = duplicate()
 		get_tree().root.add_child(clone)
 		clone.global_rotation = zone.global_rotation
 		clone.global_position = zone.global_position
+		clone.est_un_clone = true
+		clone.cible = get_global_mouse_position()
 	else:
 		print("Erreur : Impossible de trouver zonecanon. Vérifiez le nom du groupe.")
 	
 
 
 func _process(delta):
-	if Input.is_action_just_pressed("cliquegauche"):
-		cible = get_global_mouse_position()
-		return _pressed()
-	if duplicate(true):
+	if Input.is_action_just_pressed("cliquegauche") and peut_tirer and not est_un_clone:
+		peut_tirer = false # Bloque les tirs suivants
+		_pressed() # Lance le boulet
+		await get_tree().create_timer(2.0).timeout # Temps d'attente (1.0 seconde)
+		peut_tirer = true # Recharge terminée !
+	if est_un_clone == true:
 		visible = true
-		var vitesse = 50.0
+		var vitesse = 500.0
+		get_tree().create_timer(0.01).timeout
 		global_position = global_position.move_toward(cible, vitesse * delta)
+		if cible == global_position:
+			queue_free()
 		print(cible)
 # Called when the node enters the scene tree for the first time.
 
